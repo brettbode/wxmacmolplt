96cb4bbedcc4619837b8a990bf0d9fb6ad159339
schult <schult>
2006-01-13 19:51:17 +0000

    Removed MpMolData in favor of updating original MacMolPlt class for same purpose.

---
+++ src/mpGLCanvas.cpp
@@ -0,0 +1,25 @@
+/***************************************
+ * mpGLCanvas.cpp
+ *
+ * Created:       01-09-2006  Steven Schulteis
+ * Last Modified: 01-11-2006  Steven Schulteis
+ ***************************************/
+
+#include "mpGLCanvas.h"
+
+MpGLCanvas::MpGLCanvas() {
+
+}
+
+void MpGLCanvas::setMolData(const MpMolData *newMolData) {
+    molData = newMolData;
+}
+
+void MpGLCanvas::initGL(void) {
+
+}
+
+void MpGLCanvas::render(void) {
+
+}
+
--- src/mpGLCanvas.h
+++ src/mpGLCanvas.h
@@ -1,3 +1,12 @@
+/***************************************
+ * mpGLCanvas.h
+ *
+ * Description goes here.
+ *
+ * Created:       01-09-2006  Steven Schulteis
+ * Last Modified: 01-09-2006  Steven Schulteis
+ ***************************************/
+
 #ifndef MPGLCANVAS_H
 #define MPGLCANVAS_H 1
 
@@ -5,18 +14,23 @@
 
 #include "mpMolData.h"
 
+struct GLWorldData {
+};
+
 class MpGLCanvas {
     private:
-        MpMolData *molData;
-
-        void setMolData(const MpMolData *molData);
+        MpMolData   *molData;
+        GLWorldData  worldData;
 
         void initGL(void);
         void render(void);
 
     public:
         MpGLCanvas(void);
-}
+
+        // Call setMolData(NULL) before deleting the MpMolData object
+        void setMolData(const MpMolData *molData);
+};
 
 #endif
 
