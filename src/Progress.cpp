ea40f45d258c012336d3ee519c91e6bdb2577cc6
schult <schult>
2006-02-15 20:19:13 +0000

    Commented out some code in CML.cpp.

--- src/Progress.cpp
+++ src/Progress.cpp
@@ -1,3 +1,19 @@
+/*
+ * (c) 2005 Iowa State University
+ *     see the LICENSE file in the top level directory
+ */
+
+/***************************************
+ * Progress.cpp
+ *
+ * Created:       10-31-2006  Steven Schulteis
+ * Last Modified: 02-15-2006  Steven Schulteis
+***************************************/
+
+// This file currently provides no-op implementations of the functions defined
+// in Progress.h.  This is just to get things to compile for now.
+// TODO:  Write actual implementations for these functions.
+
 #include "Progress.h"
 
 Progress::Progress(void) {
