d9589b4c2ebd41edd70aa66bac7ad4e84883c540
schult <schult>
2007-04-12 21:58:48 +0000

    Created swf export options dialog (not used yet).

---
+++ src/swfexport-g.cpp
@@ -0,0 +1,179 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:        swfexport-g.cpp
+// Purpose:     
+// Author:      Brett Bode
+// Modified by: 
+// Created:     Thu 12 Apr 2007 04:16:34 PM CDT
+// RCS-ID:      
+// Copyright:   (c) 2007 Iowa State University
+// Licence:     
+/////////////////////////////////////////////////////////////////////////////
+
+#if defined(__GNUG__) && !defined(NO_GCC_PRAGMA)
+#pragma implementation "swfexport.h"
+#endif
+
+// For compilers that support precompilation, includes "wx/wx.h".
+#include "wx/wxprec.h"
+
+#ifdef __BORLANDC__
+#pragma hdrstop
+#endif
+
+#ifndef WX_PRECOMP
+#include "wx/wx.h"
+#endif
+
+////@begin includes
+////@end includes
+
+#include "swfexport.h"
+
+////@begin XPM images
+////@end XPM images
+
+/*!
+ * SWFExport type definition
+ */
+
+IMPLEMENT_DYNAMIC_CLASS( SWFExport, wxDialog )
+
+/*!
+ * SWFExport event table definition
+ */
+
+BEGIN_EVENT_TABLE( SWFExport, wxDialog )
+
+////@begin SWFExport event table entries
+////@end SWFExport event table entries
+
+END_EVENT_TABLE()
+
+/*!
+ * SWFExport constructors
+ */
+
+SWFExport::SWFExport()
+{
+	Init();
+}
+
+SWFExport::SWFExport( wxWindow* parent, wxWindowID id, const wxString& caption, const wxPoint& pos, const wxSize& size, long style )
+{
+	Init();
+	Create(parent, id, caption, pos, size, style);
+}
+
+/*!
+ * SWFExport creator
+ */
+
+bool SWFExport::Create( wxWindow* parent, wxWindowID id, const wxString& caption, const wxPoint& pos, const wxSize& size, long style )
+{
+////@begin SWFExport creation
+	SetExtraStyle(wxWS_EX_BLOCK_EVENTS);
+	wxDialog::Create( parent, id, caption, pos, size, style );
+
+	CreateControls();
+	if (GetSizer())
+	{
+		GetSizer()->SetSizeHints(this);
+	}
+	Centre();
+////@end SWFExport creation
+	return true;
+}
+
+/*!
+ * SWFExport destructor
+ */
+
+SWFExport::~SWFExport()
+{
+////@begin SWFExport destruction
+////@end SWFExport destruction
+}
+
+/*!
+ * Member initialisation 
+ */
+
+void SWFExport::Init()
+{
+////@begin SWFExport member initialisation
+	mMovieType = NULL;
+	mEPlotCheck = NULL;
+////@end SWFExport member initialisation
+}
+
+/*!
+ * Control creation for SWFExport
+ */
+
+void SWFExport::CreateControls()
+{    
+////@begin SWFExport content construction
+	SWFExport* itemDialog1 = this;
+
+	wxBoxSizer* itemBoxSizer2 = new wxBoxSizer(wxVERTICAL);
+	itemDialog1->SetSizer(itemBoxSizer2);
+
+	wxArrayString mMovieTypeStrings;
+	mMovieTypeStrings.Add(_("&Frame Animation"));
+	mMovieTypeStrings.Add(_("&Normal Mode animation"));
+	mMovieType = new wxRadioBox( itemDialog1, ID_SWF_MOVIE_RADIO, _("Radiobox"), wxDefaultPosition, wxDefaultSize, mMovieTypeStrings, 0, wxRA_SPECIFY_ROWS );
+	mMovieType->SetSelection(0);
+	itemBoxSizer2->Add(mMovieType, 0, wxALIGN_CENTER_HORIZONTAL|wxALL, 5);
+
+	mEPlotCheck = new wxCheckBox( itemDialog1, ID_EPLOT_CHECK, _("Include Energy Plot"), wxDefaultPosition, wxDefaultSize, 0 );
+	mEPlotCheck->SetValue(false);
+	if (ShowToolTips())
+		mEPlotCheck->SetToolTip(_("click to display the energy plot next to the animation for the frame animation."));
+	itemBoxSizer2->Add(mEPlotCheck, 0, wxALIGN_CENTER_HORIZONTAL|wxALL, 5);
+
+	wxBoxSizer* itemBoxSizer5 = new wxBoxSizer(wxHORIZONTAL);
+	itemBoxSizer2->Add(itemBoxSizer5, 0, wxALIGN_RIGHT|wxALL, 5);
+
+	wxButton* itemButton6 = new wxButton( itemDialog1, wxID_CANCEL, _("&Cancel"), wxDefaultPosition, wxDefaultSize, 0 );
+	itemBoxSizer5->Add(itemButton6, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5);
+
+	wxButton* itemButton7 = new wxButton( itemDialog1, wxID_OK, _("&OK"), wxDefaultPosition, wxDefaultSize, 0 );
+	itemBoxSizer5->Add(itemButton7, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5);
+
+////@end SWFExport content construction
+}
+
+/*!
+ * Should we show tooltips?
+ */
+
+bool SWFExport::ShowToolTips()
+{
+	return true;
+}
+
+/*!
+ * Get bitmap resources
+ */
+
+wxBitmap SWFExport::GetBitmapResource( const wxString& name )
+{
+	// Bitmap retrieval
+////@begin SWFExport bitmap retrieval
+	wxUnusedVar(name);
+	return wxNullBitmap;
+////@end SWFExport bitmap retrieval
+}
+
+/*!
+ * Get icon resources
+ */
+
+wxIcon SWFExport::GetIconResource( const wxString& name )
+{
+	// Icon retrieval
+////@begin SWFExport icon retrieval
+	wxUnusedVar(name);
+	return wxNullIcon;
+////@end SWFExport icon retrieval
+}
---
+++ src/swfexport.h
@@ -0,0 +1,105 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:        swfexport.h
+// Purpose:     
+// Author:      Brett Bode
+// Modified by: 
+// Created:     Thu 12 Apr 2007 04:16:34 PM CDT
+// RCS-ID:      
+// Copyright:   (c) 2007 Iowa State University
+// Licence:     
+/////////////////////////////////////////////////////////////////////////////
+
+#ifndef _SWFEXPORT_H_
+#define _SWFEXPORT_H_
+
+#if defined(__GNUG__) && !defined(NO_GCC_PRAGMA)
+#pragma interface "swfexport.h"
+#endif
+
+/*!
+ * Includes
+ */
+
+////@begin includes
+////@end includes
+
+/*!
+ * Forward declarations
+ */
+
+////@begin forward declarations
+////@end forward declarations
+
+/*!
+ * Control identifiers
+ */
+
+////@begin control identifiers
+#define ID_SWFEXPORT 10251
+#define ID_SWF_MOVIE_RADIO 10252
+#define ID_EPLOT_CHECK 10253
+#define SYMBOL_SWFEXPORT_STYLE wxCAPTION|wxRESIZE_BORDER|wxSYSTEM_MENU|wxCLOSE_BOX|wxDIALOG_MODAL
+#define SYMBOL_SWFEXPORT_TITLE _("Movie Options")
+#define SYMBOL_SWFEXPORT_IDNAME ID_SWFEXPORT
+#define SYMBOL_SWFEXPORT_SIZE wxSize(400, 300)
+#define SYMBOL_SWFEXPORT_POSITION wxDefaultPosition
+////@end control identifiers
+
+/*!
+ * Compatibility
+ */
+
+#ifndef wxCLOSE_BOX
+#define wxCLOSE_BOX 0x1000
+#endif
+
+/*!
+ * SWFExport class declaration
+ */
+
+class SWFExport: public wxDialog
+{    
+	DECLARE_DYNAMIC_CLASS( SWFExport )
+	DECLARE_EVENT_TABLE()
+
+public:
+	/// Constructors
+	SWFExport();
+	SWFExport( wxWindow* parent, wxWindowID id = SYMBOL_SWFEXPORT_IDNAME, const wxString& caption = SYMBOL_SWFEXPORT_TITLE, const wxPoint& pos = SYMBOL_SWFEXPORT_POSITION, const wxSize& size = SYMBOL_SWFEXPORT_SIZE, long style = SYMBOL_SWFEXPORT_STYLE );
+
+	/// Creation
+	bool Create( wxWindow* parent, wxWindowID id = SYMBOL_SWFEXPORT_IDNAME, const wxString& caption = SYMBOL_SWFEXPORT_TITLE, const wxPoint& pos = SYMBOL_SWFEXPORT_POSITION, const wxSize& size = SYMBOL_SWFEXPORT_SIZE, long style = SYMBOL_SWFEXPORT_STYLE );
+
+	/// Destructor
+	~SWFExport();
+
+	/// Initialises member variables
+	void Init();
+
+	/// Creates the controls and sizers
+	void CreateControls();
+
+////@begin SWFExport event handler declarations
+
+////@end SWFExport event handler declarations
+
+////@begin SWFExport member function declarations
+
+	/// Retrieves bitmap resources
+	wxBitmap GetBitmapResource( const wxString& name );
+
+	/// Retrieves icon resources
+	wxIcon GetIconResource( const wxString& name );
+////@end SWFExport member function declarations
+
+	/// Should we show tooltips?
+	static bool ShowToolTips();
+
+////@begin SWFExport member variables
+	wxRadioBox* mMovieType;
+	wxCheckBox* mEPlotCheck;
+////@end SWFExport member variables
+};
+
+#endif
+	// _SWFEXPORT_H_
