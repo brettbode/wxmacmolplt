063fcb925847f5771401a52f9ddb7ad9619098c1
brett <brett>
2008-07-30 16:31:39 +0000

    Change the style of the glew include to make VS happy

--- src/glew.cpp
+++ src/glew.cpp
@@ -29,9 +29,9 @@
 ** THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-#include <glew.h>
+#include "glew.h"
 #if defined(_WIN32)
-#  include <wglew.h>
+#  include "wglew.h"
 #elif !defined(__APPLE__) || defined(GLEW_APPLE_GLX)
 #  include <glxew.h>
 #endif
